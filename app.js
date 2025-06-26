814b28fdde8408e113c2ad88ef722201d28ab148
Ansh <141998058+Ansh9058@users.noreply.github.com>
2025-06-26 11:29:14 +0530

    Add_vehicals

--- app.js
+++ app.js
@@ -8,30 +8,25 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Home - Read all
 app.get('/', (req, res) => {
   res.render('index', { vehicles });
 });
 
-// New form
 app.get('/vehicles/new', (req, res) => {
   res.render('new');
 });
 
-// Create
 app.post('/vehicles', (req, res) => {
   const { vehicleName, price, image, desc, brand } = req.body;
   vehicles.push({ id: id++, vehicleName, price, image, desc, brand });
   res.redirect('/');
 });
 
-// Edit form
 app.get('/vehicles/edit/:id', (req, res) => {
   const vehicle = vehicles.find(v => v.id == req.params.id);
   res.render('edit', { vehicle });
 });
 
-// Update
 app.post('/vehicles/update/:id', (req, res) => {
   const { vehicleName, price, image, desc, brand } = req.body;
   const index = vehicles.findIndex(v => v.id == req.params.id);
@@ -39,13 +34,11 @@ app.post('/vehicles/update/:id', (req, res) => {
   res.redirect('/');
 });
 
-// Delete
 app.post('/vehicles/delete/:id', (req, res) => {
   vehicles = vehicles.filter(v => v.id != req.params.id);
   res.redirect('/');
 });
 
-// API endpoint for JSON
 app.get('/api/vehicles', (req, res) => {
   res.json(vehicles);
 });
